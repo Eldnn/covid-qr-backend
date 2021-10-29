6c873bd2a4a2ec8171a026a0d92611098172f272
Adrian Jörg <adi.joerg@hotmail.com>
2021-10-29 10:52:37 +0200

    Add support for Light cert

--- index.js
+++ index.js
@@ -39,13 +39,14 @@ let trustListLastUpdate = {'keys': -1, 'updates': -1};
 app.post('/verify', (req, res) => {
     console.log(req.body);
     let cert = req.body.cert;
+    const isLight = cert.startsWith('LT');
     cert = cert.slice(4);
     const base45decoded = base45.decode(cert);
     const _coseRaw = zlib.inflateSync(base45decoded);
     let [headers1, headers2, cborPayload, signature] = cbor.decodeFirstSync(_coseRaw).value;
     const kid = cbor.decodeFirstSync(headers1).get(4).toString('base64');
     const jsonCBOR = cbor.decodeFirstSync(cborPayload);
-    const _payload = jsonCBOR.get(-260).get(1);
+    const _payload = isLight ? jsonCBOR.get(-250).get(1) : jsonCBOR.get(-260).get(1);
 
     try {
         let verifier;
@@ -139,10 +140,16 @@ function updateUpdates() {
 }
 
 function checkRulesAndCreateObject(data) {
-    const cert_data = data.get(-260).get(1);
+    const isLight = data.get(-250) ? true : false;
+    if (isLight) {
+        const cert_data = data.get(-250).get(1);
+        
+        return {type: 'L', iss: data.get(1), iss_at: data.get(6)*1000, data: cert_data, valid: Date.now() < data.get(4)*1000}
+    }
+    const cert_data =  data.get(-260).get(1);
     const type = cert_data.v ? 'V' : cert_data.t ? 'T' : 'R';
     const iss = data.get(1);
-    const iss_at = data.get(6);
+    const iss_at = data.get(6)*1000;
     let valid = true;
 
     if (type === 'V'){
@@ -158,7 +165,7 @@ function checkRulesAndCreateObject(data) {
 
 function validateVacc(cert_data) {
     // Version 1.4 (https://ec.europa.eu/health/sites/default/files/ehealth/docs/digital-green-value-sets_en.pdf)
-    let vacc_date = cert_data.dt ? new Date(cert_data.dt) : new Date(0);
+    let vacc_date = cert_data.dt ? new Date(cert_data.dt*1000) : new Date(0);
     valid = 
         (cert_data.tg && cert_data.tg === '840539006') && // Vaccinated against covid
         (cert_data.vp && ['1119305005', '1119349007', 'J07BX03'].indexOf(cert_data.vp) > -1) && //Type of the vaccine or prophylaxis used
@@ -169,7 +176,7 @@ function validateVacc(cert_data) {
 }
 
 function validateTest(cert_data) {
-    let test_time = cert_data.sc ? new Date(cert_data.sc) : new Date(0);
+    let test_time = cert_data.sc ? new Date(cert_data.sc*1000) : new Date(0);
     valid = 
         (cert_data.tg && cert_data.tg === '840539006') && 
         (cert_data.tt && ['LP6464-4', 'LP217198-3'].indexOf(cert_data.tt) > -1) && 
@@ -180,7 +187,7 @@ function validateTest(cert_data) {
 }
 
 function validateRec(cert_data) {
-    let test_time = cert_data.fr ? new Date(cert_data.fr) : new Date(0);
+    let test_time = cert_data.fr ? new Date(cert_data.fr*1000) : new Date(0);
     let time_diff = new Date() - test_time;
     valid = 
         (cert_data.tg && cert_data.tg === '840539006') && 
